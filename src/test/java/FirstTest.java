87679c4347ded0adf802db805ee3d2ee0d975e2d
OlegQA31 <ol-dyom@yandex.ru>
2022-07-05 21:44:47 +0300

    Chrome and Edge browsers

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,11 +1,11 @@
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
-public class FirstTest extends InitTest{
-@Test
-    public void loginSuccessTest(){
-    new LoginPage(wd)
-            .fillInTheForm("qaorg","admin","Qwerty123")
-            .confirmLogin();
-}
+public class FirstTest extends InitTest {
+    @Test
+    public void loginSuccessTest() {
+        new LoginPage(wd)
+                .fillInTheForm("qaorg", "admin", "Qwerty123")
+                .confirmLogin();
+    }
 }
--- src/test/java/InitTest.java
+++ src/test/java/InitTest.java
@@ -1,18 +1,31 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class InitTest {
+
+    String browser=System.getProperty("browser", BrowserType.CHROME);
     WebDriver wd;
+
     @BeforeSuite
     public void init(){
-        WebDriverManager.chromedriver().setup();
-        wd=new ChromeDriver();
+        if(browser.equals(BrowserType.CHROME)){
+            WebDriverManager.chromedriver().setup();
+            wd=new ChromeDriver();
+
+        }else if(browser.equals(BrowserType.EDGE)){
+            WebDriverManager.edgedriver().setup();
+            wd=new EdgeDriver();
+        }
+
         wd.manage().window().maximize();
         wd.navigate().to("https://iot.plataine.com/#/Login");
     }
-
+    @AfterSuite
     public void tearDown(){
         //wd.quit();
     }
